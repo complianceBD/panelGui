8e53dfa951a177c1239a79957b855d0a9203a2e3
Kramer <Michael.Kramer@bnymellon.com>
2018-07-05 10:15:40 -0400

    added open low priced report button

--- TabTwo.py
+++ TabTwo.py
@@ -2,6 +2,16 @@ import wx
 import glob
 from bnyCompliance.equity.lowPriceSec import executedOrderReport, combineFiles
 import os
+import win32com.client as win32
+from bnyCompliance.ReportOpener.excelcomm import openWorkbook
+
+
+
+
+ReportDirs = {
+    "LowPriceReportDir":"H:\\Post June 11, 2010\\Equity Low Priced Report"
+
+}
 
 class LowPriceSec(wx.Panel):
     def __init__(self, parent):
@@ -34,13 +44,10 @@ class LowPriceSec(wx.Panel):
 
         save = "H://Post June 11, 2010//Equity Low Priced Report//" #the directory where the final output is saved
 
-        os.chdir('T://CMI//MUNI//FidessaComplianceReportingBKCM') 
-        all_subdirs = [d for d in os.listdir('.') if os.path.isdir(d)]
-
-        latest_subdir = max(all_subdirs, key=os.path.getmtime)
-        reportdir = os.chdir('./'+latest_subdir)
-        reportdir = os.getcwd()
-        orderReports = glob.glob(reportdir + "\\EXECUTED_ORDER*")
+        PATH_TO_FIDESSA = os.path.abspath('T://CMI//MUNI//FidessaComplianceReportingBKCM//')
+        dir_list = [os.path.join(PATH_TO_FIDESSA, d) for d in os.listdir(PATH_TO_FIDESSA) if os.path.isdir(os.path.join(PATH_TO_FIDESSA, d))]
+        latest_subdir = max(dir_list, key=os.path.getmtime)
+        orderReports = glob.glob(latest_subdir + "\\EXECUTED_ORDER*")
         
         adv = self.advThreshold.GetValue()
         price = self.priceThreshold.GetValue()
@@ -55,6 +62,64 @@ class LowPriceSec(wx.Panel):
             rpt = executedOrderReport(orderReports[-1], save,int(price), int(adv))
             rpt.save()
 
+class TabTwoExcelOpen(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, style=wx.SUNKEN_BORDER)
+
+        
+        
+        #---------------------------------------------------------
+        instructionText =  wx.StaticText(self, label="OPEN REPORTS IN EXCEL")
+        font = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+        instructionText.SetFont(font)
+        #--------------------MTG Best Ex Open Button---------------------------
+     
+        #File Lcoation 
+        LowPricedSecText = wx.StaticText(self, label="Open Most recent 'Equity Low Priced Security Report' (must run the report first): ")
+        LowPriceSecOpenButton = wx.Button(self, label="Open Low Priced Security Report")
+        LowPriceSecOpenButton.Bind(wx.EVT_BUTTON, self.OpenFileLowPriceReport)
+       
+        #--------------------Sizer----------------------------------------------
+        sizer = wx.GridBagSizer(7, 5)
+        sizer.Add(instructionText, pos=(0,0), flag=wx.LEFT)
+        
+        sizer.Add(LowPricedSecText, pos=(2, 0), flag=wx.LEFT, border=10) #Date static text nested on left
+        sizer.Add(LowPriceSecOpenButton, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)#mtgBestEx Button
+        self.SetSizer(sizer)
+        
+        
+        
+        #------------------------------- Opens most recent low price sec report in directory------------
+        
+    def OpenFileLowPriceReport(self, event):
+    
+        
+        dlg = wx.MessageDialog(self, "Open Report",
+                       style=wx.DD_DEFAULT_STYLE)
+            
+            
+        if dlg.ShowModal() == wx.ID_OK:
+            try:
+                PATH_TO_DIR = os.path.abspath(ReportDirs['LowPriceReportDir'])
+                LIST_REPORTS = os.listdir(PATH_TO_DIR)[-1]
+                PATH_TO_RPT = os.path.join(PATH_TO_DIR, LIST_REPORTS)
+                print('The most recent report is: \n'+LIST_REPORTS)
+                excel = win32.gencache.EnsureDispatch('Excel.Application')
+                wb =  openWorkbook(excel, PATH_TO_RPT)
+                ws = wb.Worksheets('Sheet1') 
+                excel.Visible = True
+
+            except Exception as e:
+                print(e)
+
+            finally:
+                # RELEASES RESOURCES
+                ws = None
+                wb = None
+                excel = None
+
+
+
 
 class EquityTabMain(wx.Panel):
 
@@ -62,9 +127,9 @@ class EquityTabMain(wx.Panel):
         """Constructor"""
         wx.Panel.__init__(self, parent, style=wx.SUNKEN_BORDER)
         panel = LowPriceSec(self)
-        #panel2 = use for more reports
+        panel2 = TabTwoExcelOpen(self)
         
         sizer = wx.GridBagSizer(10,5)
         sizer.Add(panel, pos=(2,1))
-        #sizer.Add(panel2, pos=(4,1)) us for more reports later
+        sizer.Add(panel2, pos=(4,1))
         self.SetSizer(sizer)
---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import wx
+import wx.lib.agw.multidirdialog as MDD
+from TabOne import FixedIncomeTabMain
+from TabTwo import EquityTabMain
+from TabThree import TkgTabMain
+from bnyCompliance.ReportOpener.OpenFile import OPEN_MORT, OPEN_TSY
+#from TabFour import ReportGrid
+
+
+       
+class MainFrame(wx.Frame):
+    """"""
+
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        
+        screenSize = wx.DisplaySize()
+        screenWidth = screenSize[0]
+        screenHeight = screenSize[1]
+        wx.Frame.__init__(self, None, title="Comp Mgr", size = (1500,1000))
+        
+
+        nb = wx.Notebook(self)
+        nb.SetBackgroundColour('#dee0e2')
+        
+        tab1 = FixedIncomeTabMain(nb)
+        tab2 = EquityTabMain(nb)
+        tab3 = TkgTabMain(nb)
+        #tab4 = ReportGrid(nb)
+        
+        nb.AddPage(tab1, "Fixed Income Reports")
+        nb.AddPage(tab2, "Equity Reports")
+        nb.AddPage(tab3, "TKG")
+        #nb.AddPage(tab4, "Report Viewer")
+ 
+        
+        #panel = TabOneMainPanel(self)
+
+        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+
+        self.SetTitle('Compliance Manager')
+        
+        sizer = wx.BoxSizer()
+        sizer.Add(nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+        self.Show()
+        
+     ############################################
+        """Window close warning"""
+
+     ###############################################
+    def OnCloseWindow(self, e):
+
+        dial = wx.MessageDialog(None, 'Are you sure to quit?', 'Question',
+            wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+
+        ret = dial.ShowModal()
+
+        if ret == wx.ID_YES:
+            self.Destroy()
+        else:
+            e.Veto()
+ 
+if __name__ == "__main__":
+    OPEN_MORT
+    OPEN_TSY
+    app = wx.App()
+    MainFrame().Show()
+    app.MainLoop()
+    
+  
